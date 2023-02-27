5614c6ca4e67c60224c4f88f7a02db55057b984f
Uranta <vivianchijindu96@gmail.com>
2023-02-27 14:41:40 -0800

    my first commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,20 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
  * reset_to_98 - function that takes a pointer to an int as parameter and
  * updates the value it points to to 98.
  *
- * @n: This is the entry
+ * return: always 0.
  *
  */
 
 void reset_to_98(int *n)
 {
-	*n = 98;
+        *n = 98;
+
 }
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-reset_to_98.c -o 0-98
+./0-98
+n=402
+n=98
