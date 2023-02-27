a6a296c34ef491de6c49cd94975b37d5a79a0cf2
Uranta <vivianchijindu96@gmail.com>
2023-02-27 15:43:11 -0800

    tasks done

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,24 +2,24 @@
 #include <stdio.h>
 
 /**
- * puts2 - Write a function that prints every other character of a string, 
- *         starting with the first character, followed by a new line.
+ * puts2 - Prints every other character of a string, starting with the first.
  *
- * @s: This pointer point to a char or string
+ * @s: A pointer to the string to print.
  *
- *
+ * Description: This function prints every other character of a string,
+ * starting with the first character, followed by a new line.
  */
-
 void puts2(char *s)
 {
-	int a;
+        int a;
 
-	for (a = 0; s[a] != '\0'; a++)
-	{
-		if (a % 2 == 0)
-		{
-			_putchar(s[a]);
-		}
-	}
-	_putchar('\n');
+        for (a = 0; s[a] != '\0'; a++)
+        {
+                if (a % 2 == 0)
+                {
+                        _putchar(s[a]);
+                }
+        }
+        _putchar('\n');
 }
+
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,15 +12,16 @@
 
 void print_array(int *a, int n)
 {
-	int index;
-	
-	for (index = 0; index < n; index++)
-	{
-		printf("%d", a[index]);
-		if (index != (n - 1))
-		{
-			printf(", ");
-		}
-	}
-	putchar('\n');
+    int index;
+
+    for (index = 0; index < n; index++)
+    {
+        printf("%d", a[index]);
+        if (index != (n - 1))
+        {
+            printf(",");
+        }
+    }
+    putchar('\n');
 }
+
