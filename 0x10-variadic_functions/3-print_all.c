4eef18e966fe613eebba495f7a63481679ff6cd1
Uranta <vivianchijindu96@gmail.com>
2023-03-23 15:37:19 -0700

    final commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,83 +1,54 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "variadic_functions.h"
-/**
- * print_i - prints int
- * @list: arguement of list
- * @s: seperator
- * Return: none
- */
-void print_i(va_list list, char *s)
-{
-	printf("%s%d", s, va_arg(list, int));
-}
-/**
- * print_c - prints char
- * @list: arguement char
- * @sep: seperator
- */
-void print_c(va_list list, char *sep)
-{
-	printf("%s%c", sep, va_arg(list, int));
-}
-/**
- * print_s - prints string
- * @sep: seperator
- * @list: list to print
- * Return: none
- */
-void print_s(va_list list, char *sep)
-{
-	char *s;
+#include <stdarg.h>
 
-	s = va_arg(list, char *);
-	if (s == NULL)
-		s = "(nil)";
-	printf("%s%s", sep, s);
-}
 /**
- * print_f - prints floats
- * @sep: float to print
- * @list: next arguement of list to print
- * Return: none
+ * print_all - prints arguments of various types, based on a format string
+ * @format: a string containing the format specifiers for the arguments
+ *          'c' for char, 'i' for int, 'f' for float, 's' for string
+ *          any other character is ignored
+ *          format cannot be NULL
+ * @...: a variable number of arguments to be printed, one for each format specifier
  */
-void print_f(va_list list, char *sep)
+void print_all(const char *const format, ...)
 {
-	printf("%s%f", sep, va_arg(list, double));
+    va_list args;
+    int i = 0;
+    char c;
+    float f;
+    char *s;
+
+    va_start(args, format);
+
+    while (format && format[i])
+    {
+        switch (format[i])
+        {
+            case 'c':
+                c = va_arg(args, int);
+                printf("%c", c);
+                break;
+            case 'i':
+                i = va_arg(args, int);
+                printf("%d", i);
+                break;
+            case 'f':
+                f = va_arg(args, double);
+                printf("%f", f);
+                break;
+            case 's':
+                s = va_arg(args, char *);
+                if (s)
+                    printf("%s", s);
+                else
+                    printf("(nil)");
+                break;
+        }
+        if (format[i + 1])
+            printf(",");
+        i++;
+    }
+
+    printf("\n");
+    va_end(args);
 }
-/**
- * print_all - prints out all stuff
- * @format: format is list of types of arguements
- */
-void print_all(const char * const format, ...)
-{
-	va_list list;
-	char *sep;
-	int i, j;
-	fm_t fm[] = {
-		{"c", print_c},
-		{"i", print_i},
-		{"f", print_f},
-		{"s", print_s},
-		{NULL, NULL}
-	};
-	va_start(list, format);
-	i = 0;
-	sep = "";
-	while (format != NULL && format[i] != '\0')
-	{
-		j = 0;
-		while (j < 4)
-		{
-			if (format[i] == *(fm[j]).fm)
-			{
-				fm[j].p(list, sep);
-				sep = ", ";
-			}
-			j++;
-		}
-		i++;
-	}
-	printf("\n");
-	va_end(list);
-}
+
